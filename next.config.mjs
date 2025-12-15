d1683820b3b9aa29de2711e5995b7087a83f9249
Lodhi008 <92319238+Lodhi008@users.noreply.github.com>
2025-12-15 08:33:01 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.educative.io',
-        port: '',
-        pathname: '/**', // allow all paths under this domain
-      },
-    ],
+    domains: ['www.educative.io'],
   },
 };
 
